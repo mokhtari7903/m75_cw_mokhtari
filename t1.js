62d3c86b6b2f585d7de9de9d17b3176106969a96
mokhtari7903 <mokhtari7903@gmail.com>
2022-04-09 08:17:40 +0430

    m75_cw5

---
+++ t1.js
@@ -0,0 +1,12 @@
+let sentence1 = prompt('your sentence: ');
+let word1 = prompt('enter your word: ')
+function findWord(word, sentence){
+
+let aSen = sentence.split(' ');
+
+const isEx = aSen.filter((item) => { return item === word});
+
+return isEx[0] === word;
+
+}
+console.log(findWord(word1, sentence1));
