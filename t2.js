82a408ca620b8509d01bbeb89c20330ae4be5ce0
mokhtari7903 <mokhtari7903@gmail.com>
2022-04-09 08:18:40 +0430

    m75_cw5

---
+++ t2.js
@@ -0,0 +1,6 @@
+let alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','W','X','Y','Z'];
+let new1 = alphabet.filter((item, i) => {return i % 2 === 0 }
+
+  
+);
+console.log(new1)
