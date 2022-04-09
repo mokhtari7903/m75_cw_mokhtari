35e6539affb4789a853daac3628a1cd32a34a9b6
mokhtari7903 <mokhtari7903@gmail.com>
2022-04-09 08:19:23 +0430

    m75_cw5

---
+++ t3.js
@@ -0,0 +1,15 @@
+let listName = [{name: 'ali', lName: 'faraji'},{name: 'reza', lName: 'zamany'}, ];
+let nArray = listName.map((item) => { return `${item.name} ${item.lName}` });
+console.log(nArray);
+    
+
+
+
+/* Follow : (20 min)
+function to test whether a date is a weekend
+function to get difference between two dates in days
+function to calculate 'yesterday day' 'previous month' 'next month'
+function to get the day of the month, 2 digits with leading zeros
+function to get time uppercase Ante meridiem and Post meridiem (AM,PM)
+function to get time differences in minutes between two dates
+ */
